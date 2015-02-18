8ea5061d7e96f6637a4fd5cf18eaa765b79fcad0
Alexander Voitenko <voitenko@gmail.com>
2015-02-18 15:12:44 +0200

    part 6. User profile

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,15 @@
 __author__ = 'stclaus'
 
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
-from wtforms.validators import Required, DataRequired
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import Required, DataRequired, Length
 
 
 class LoginForm(Form):
     openid = StringField('openid', validators=[DataRequired()])
     remember_me = BooleanField('remember_me', default=False)
+
+
+class EditForm(Form):
+    nickname = StringField('nickname', validators=[DataRequired()])
+    about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
